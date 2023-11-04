51b5e855e3ff6012a71447b91599881d564e6797
Yumi Yukimura <me.cafebabe@gmail.com>
2023-11-05 02:20:10 +0800

    Mi8937: Import ugg goodixfp firmwares from stock /system/etc/firmware/
    * Change the firmware location to /odm/firmware/ugg/ while we're at it
    
    Change-Id: I6ef638ca5ec09e0e09c20fe0294cf178a2975f12

--- Mi8937-vendor.mk
+++ Mi8937-vendor.mk
@@ -904,6 +904,14 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/acdbdata/santoni/QRD_SKU7_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/santoni/QRD_SKU7_Hdmi_cal.acdb \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/acdbdata/santoni/QRD_SKU7_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/santoni/QRD_SKU7_Headset_cal.acdb \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/acdbdata/santoni/QRD_SKU7_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/santoni/QRD_SKU7_Speaker_cal.acdb \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b00:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b00 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b01:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b01 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b02:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b02 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b03:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b03 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b04:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b04 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b05:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b05 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.b06:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.b06 \
+    vendor/xiaomi/Mi8937/proprietary/odm/firmware/ugg/goodixfp.mdt:$(TARGET_COPY_OUT_ODM)/firmware/ugg/goodixfp.mdt \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/qdcm_calib_data_ili9881c_HD720p_video_Ebbg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881c_HD720p_video_Ebbg.xml \
     vendor/xiaomi/Mi8937/proprietary/vendor/etc/qdcm_calib_data_ili9881c_HD720p_video_EbbgDJN.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881c_HD720p_video_EbbgDJN.xml \
